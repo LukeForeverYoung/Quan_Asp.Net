939ad1549f2fe2c360b1acc2f270a51725ab2b07
Luke <mizukiluke@outlook.com>
2018-01-26 11:34:24 +0800

    init

---
+++ WebApplication1/Controllers/DefaultController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class DefaultController : ApiController
+    {
+    }
+	public class UserController:ApiController
+	{
+		public IHttpActionResult GetUser(int id)
+		{
+			
+		}
+	}
+	public class ProductsController : ApiController
+	{
+		Product[] products1 = new Product[]
+		{
+			new Product { Id = 1, Name = "Tomato Soup", Category = "Groceries", Price = 1 },
+			new Product { Id = 2, Name = "Yo-yo", Category = "Toys", Price = 3.75M },
+			new Product { Id = 3, Name = "Hammer", Category = "Hardware", Price = 16.99M }
+		};
+
+		public IEnumerable<Product> GetAllProducts()
+		{
+			return products1;
+		}
+
+		public IHttpActionResult GetProduct(int id)
+		{
+			var product = products1.FirstOrDefault((p) => p.Id == id);
+			if (product == null)
+			{
+				return NotFound();
+			}
+			return Ok(product);
+		}
+	}
+}
---
+++ WebApplication1/distance.js
@@ -0,0 +1,37 @@
+﻿function rad(d) {
+    return d * Math.PI / 180.0;
+}
+//纬度，经度
+function GetDistance(lat1, lng1, lat2, lng2) {
+    var radLat1 = rad(lat1);
+    var radLat2 = rad(lat2);
+    var a = radLat1 - radLat2;
+    var b = rad(lng1) - rad(lng2);
+    var s = 2 * Math.asin(Math.sqrt(Math.pow(Math.sin(a / 2), 2) +
+        Math.cos(radLat1) * Math.cos(radLat2) * Math.pow(Math.sin(b / 2), 2)));
+    s = s * 6378.137;// EARTH_RADIUS;
+    s = Math.round(s * 10000) / 10000;
+    return s;
+}
+/*
+function calDis() {
+    var lat1 = document.getElementById("lat1").value * 1;
+    var lat2 = document.getElementById("lat2").value * 1;
+    var lng1 = document.getElementById("lng1").value * 1;
+    var lng2 = document.getElementById("lng2").value * 1;
+    var dis = GetDistance(lat1, lng1, lat2, lng2);
+    document.getElementById("distance").value = dis;
+}
+*/
+function getdis () {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function (pos) {
+            var coords = pos.coords;
+            console.log(coords);
+            var dis = GetDistance(coords.latitude, coords.longitude, 26.22, 111.63);//永州经纬度(维度在前,经度在后)
+            window.location.href = "addQuest.html" + "?dis=" + dis;
+        });
+    }
+    else
+        console.log("获取失败，请检查是否支持位置信息读取。");
+}
